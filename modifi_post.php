02475181d384d77e4bcd40f4e35f0e4c268e771b
justine <justine.telmann@gmail.com>
2019-05-14 15:51:32 +0200

    ajout modif mdp

--- modifi_post.php
+++ modifi_post.php
@@ -1,7 +1,10 @@
 <?php 
-$passmod=$_POST['passmod'];
-$passmodconf=$_POST['passmodconf'];
-$pass=$_POST['pass'];
+// include("connection_post.php");
+session_start();
+$passmod= md5($_POST['passmod']);
+$passmodconf= md5($_POST['passmodconf']);
+$pass= md5($_POST['pass']);
+$id = $_SESSION["id"];
 
 try{
     $bdd = new PDO('mysql:host=localhost;dbname=inscription;charset=utf8', 'root', '');
@@ -11,21 +14,36 @@ try{
     {
         die('erreur : ' . $e->getMessage());
     }
+
+     
 // verifier la presence des variable et si elle ne sont pas vide
 if (isset($_POST['pass']) AND isset($_POST['passmod']) AND isset($_POST['passmodconf']))
     {   
         if (!empty($_POST['pass']) AND !empty($_POST['passmod']) AND !empty($_POST['passmodconf']))
         {
-            if($pass == crypt(($_POST['pass']), $salt))
-        }
-                else{
-                    
-                        $req = $bdd->prepare('UPDATE pass SET pass = :passmod ');
-                        $req->execute(array(
-	                                    'pass' => $passmod
-	                                    ));
+            $req = $bdd->prepare('SELECT pass FROM membre WHERE id = :id');
+            $req->execute(array(
+                'id' => $id));
 
+            $resultat = $req->fetch();
+            var_dump($resultat);
+            if($resultat["pass"] != $pass){
+                 echo 'mots de passe incorrect';
+            }
+            else{
+                if($passmod == $passmodconf){
+                    $req = $bdd->prepare('UPDATE membre SET pass = :passmod WHERE id = :id');
+                    $req->execute(array(
+                        'passmod' => $passmod,
+                        'id' => $id
+                    ));
+                    header('Location: login.php');
                 }
+            }
+
+                  
+                }
+            
             }
         
     ?>
